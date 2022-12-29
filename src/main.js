f8d5008633fb9596b406f7b810e3abe83bad090a
Avaz Shukurullayev <elonarm21@gmail.com>
2022-12-29 16:39:55 +0500

    fix bugs

--- src/main.js
+++ src/main.js
@@ -2,11 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./global.scss";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import "./global.scss";
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
