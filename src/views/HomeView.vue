80761eac20a0612e19f42d1feb5141b7ee7f8983
Avaz Shukurullayev <elonarm21@gmail.com>
2023-01-04 15:00:25 +0500

    files are changed'

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@
       <div class="row">
         <div class="col-lg-3 shadow my-5 p-3 rounded">
           <b-form @submit.prevent="onSubmit" @reset.prevent="onReset">
+            <h2 class="form__title">Registration 😎</h2>
             <b-form-group
               class="my-2"
               label="Enter your firstname"
@@ -41,18 +42,23 @@
                 required
               ></b-form-input>
             </b-form-group>
+
             <b-form-group
               class="my-2"
               label="Enter your password"
               label-for="password"
             >
               <b-form-input
+                @input="onPassword"
                 type="password"
                 v-model="form.password"
                 id="password"
                 placeholder="*******"
                 required
               ></b-form-input>
+              <p class="password__parag least">At least 5 letters</p>
+              <p class="password__parag number">At least one number</p>
+              <p class="password__parag word">At least one word</p>
             </b-form-group>
             <b-form-group
               class="my-2"
@@ -60,18 +66,24 @@
               label-for="confirm-password"
             >
               <b-form-input
+                @input="onConfirmPassword"
                 type="password"
                 id="confirm-password"
                 v-model="form.confirmPassword"
                 placeholder="*******"
                 required
               ></b-form-input>
+              <p class="password__desc unSame">Not same password 😒</p>
+              <p class="password__desc same">Same password, Correct ✅</p>
             </b-form-group>
             <b-button type="submit" class="my-2" variant="primary"
               >Submit</b-button
             >
-            <b-button type="reset" class="my-2" variant="danger"
+            <b-button type="reset" class="mx-2" variant="danger"
               >Reset</b-button
+            >
+            <b-button @click="showPassword" class="showButton" variant="info"
+              >Show</b-button
             >
           </b-form>
         </div>
@@ -93,69 +105,90 @@ export default {
   data() {
     return {
       form: {},
+      check: {
+        password: "",
+        confirmPassword: "",
+        same: null,
+      },
     };
   },
   computed: {
     ...mapGetters(["getForm"]),
   },
-  mounted(){
-    console.log(this.getForm);
+  mounted() {
+    console.log("Mounted methods => ", this.getForm);
   },
   methods: {
     ...mapActions(["actionForm"]),
     onSubmit() {
-      this.actionForm(this.form);
-      this.form = {};
+      if (this.check.same) {
+        this.actionForm(this.form);
+        this.form = {};
+      } else {
+        alert("onSubmit else statement");
+      }
     },
     onReset() {
       this.form = {};
+    },
+    onPassword() {
+      const passwordParag = document.querySelectorAll(".password__parag");
+      const passwordLeast = document.querySelector(".least");
+      const passwordNumber = document.querySelector(".number");
+      const passwordWord = document.querySelector(".word");
+      if (this.form.password != "") {
+        passwordParag.forEach((element) => (element.style.display = "block"));
+        if (isNaN(this.form.password)) {
+          console.log("isNaN => ", isNaN(this.form.password));
+          passwordWord.style.display = "none";
+          passwordNumber.style.display = "none";
+          if (this.form.password.length >= 5) {
+            passwordLeast.style.display = "none";
+            this.check.password = this.form.password;
+          }
+        }
+      }
+    },
+    onConfirmPassword() {
+      const unSame = document.querySelector(".unSame");
+      const same = document.querySelector(".same");
+      if (this.form.password === this.form.confirmPassword) {
+        alert("Successfully passwords");
+        unSame.style.display = "none";
+        same.style.display = "block";
+        this.check.same = true;
+      } else {
+        console.log("unsame passwords");
+        unSame.style.display = "block";
+        same.style.display = "none";
+        this.check.same = false;
+      }
+    },
+    showPassword() {
+      if (this.form.password !== "" || this.form.confirmPassword !== "") {
+        this.form.password.type = "text";
+        this.form.confirmPassword.type = "text";
+      } else {
+        this.form.password.type = "password";
+        this.form.confirmPassword.type = "password";
+      }
     },
   },
 };
 </script>
 
-<!-- <script>
-export default {
-  name: "HomeView",
-  components: {},
-  data() {
-    return {
-      form: {
-        email: "",
-        name: "",
-        food: null,
-        checked: [],
-      },
-      foods: [
-        { text: "Select One", value: null },
-        "Carrots",
-        "Beans",
-        "Tomatoes",
-        "Corn",
-      ],
-      show: true,
-    };
-  },
-  methods: {
-    onSubmit(event) {
-      event.preventDefault();
-      alert(JSON.stringify(this.form));
-    },
-    onReset(event) {
-      event.preventDefault();
-      // Reset our form values
-      this.form.email = "";
-      this.form.name = "";
-      this.form.food = null;
-      this.form.checked = [];
-      // Trick to reset/clear native browser form validation state
-      this.show = false;
-      this.$nextTick(() => {
-        this.show = true;
-      });
-    },
-  },
-};
-</script>
- -->
-<style></style>
+<style lang="scss">
+.form__title {
+  text-align: center;
+  margin: 10px 0;
+}
+.password__parag {
+  color: red;
+  line-height: 25px;
+  display: none;
+}
+.password__desc {
+  color: red;
+  display: none;
+}
+</style>
